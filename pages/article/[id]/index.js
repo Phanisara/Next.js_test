2c65a643180f38dee3a80b622ab2bbb4ddf67503
Phanisara <mild.mm38@gmail.com>
2022-09-29 12:02:44 +0700

    update

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -9,35 +9,35 @@ const article = ({article}) => {
 
   return (
     <>
-        <Meta title={article.title} description={article.excerpt}/>
-        <h2>{article.title}</h2>
-        <p>{article.body}</p>
+        <Meta title={article} description={article}/>
+        <h2>{article}</h2>
+        <p>{article}</p>
         <br />
         <Link href={'/'}>Go Back</Link>
     </>
   )
 }
 
-export const getStaticProps = async (context) => {
-    const res = await fetch(`${server}/api/articles/${context.params.id}`)
-    const article = await res.json()
-    return {
-        props:{
-            article
-        }
-    }
-}
+// export const getStaticProps = async (context) => {
+//     const res = await fetch(`${server}/api/articles/${context.params.id}`)
+//     const article = await res.json()
+//     return {
+//         props:{
+//             article
+//         }
+//     }
+// }
 
-export const getStaticPaths = async (context) => {
-    const res = await fetch(`${server}/api/articles`)
-    const articles = await res.json()
-    const ids = articles.map(article => article.id)
-    const paths = ids.map(id => ({params: {id: id.toString()}}))
-    return {
-        paths,
-        fallback: false
-    }
-}
+// export const getStaticPaths = async (context) => {
+//     const res = await fetch(`${server}/api/articles`)
+//     const articles = await res.json()
+//     const ids = articles.map(article => article.id)
+//     const paths = ids.map(id => ({params: {id: id.toString()}}))
+//     return {
+//         paths,
+//         fallback: false
+//     }
+// }
 
 // export const getStaticProps = async (context) => {
 //     const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
